cfe870467e7d15e290e7fef5692107ef9c1f65fa
wouyang628 <36004307+wouyang628@users.noreply.github.com>
2018-03-26 17:49:22 -0400

    Update user_functions.py

--- user_functions.py
+++ user_functions.py
@@ -5,11 +5,15 @@ from jinja2 import Environment, FileSystemLoader
 import datetime
 import time
 import requests
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
 
 node_url = 'http://10.10.2.54:8091/NorthStar/API/v1/tenant/1/topology/1/nodes'
 lsp_url = 'http://10.10.2.54:8091/NorthStar/API/v2/tenant/1/topology/1/te-lsps'
 maintenance_url = 'http://10.10.2.54:8091/NorthStar/API/v2/tenant/1/topology/1/maintenances'
 headers = {'Authorization': 'Bearer Z28XMqGf4EPJvfDjGbAWRFp7LPXGJZzHO33mFbcJO6I=', 'Content-Type': 'application/json'}
+user = ''
+password = ''
 
 
 def get_node_info(hostname):
@@ -57,7 +61,8 @@ def generate_node_maitenance_json(index_number):
     j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
                          trim_blocks=True)
 
-    payload = j2_env.get_template('node_maintenance.j2').render(
+    payload = j2_env.get_template('maintenance.j2').render(
+        maintenance_type='node',
         index_number=index_number,
         current_time=datetime.datetime.utcnow().strftime("%Y%m%d%H%M"),
         start_time=getTimeSeqUTC(1),
@@ -72,8 +77,9 @@ def generate_link_maitenance_json():
     j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
                          trim_blocks=True)
 
-    payload = j2_env.get_template('link_maintenance.j2').render(
-        index_number=6,
+    payload = j2_env.get_template('maintenance.j2').render(
+        maintenance_type='link',
+        index_number=17,
         current_time=datetime.datetime.utcnow().strftime("%Y%m%d%H%M"),
         start_time=getTimeSeqUTC(1),
         end_time=getTimeSeqUTC(6000)
@@ -92,3 +98,29 @@ def getTimeSeqUTC(num):
     endstr = "00"
     finalTime = ''.join([juniorTime, endstr])
     return finalTime + 'Z'
+
+
+def set_overload_bit(router_to_configure):
+    global user
+    global password
+    print "making confugration changes on " + router_to_configure
+    print "#####################################"
+    dev = Device(host=router_to_configure, user=user, password=password).open()
+    with Config(dev) as cu:
+        cu.load('set protocols isis overload', format='set')
+        cu.pdiff()
+        cu.commit()
+
+
+def get_management_ip(host_name):
+    network_info = './network_device.json'
+    data = json.loads(open(network_info).read())
+    for i in data['NetworkDeviceList']:
+        # pprint(i)
+        # i['NetworkDevice']['Name']
+        if i['NetworkDevice']['Name'] == host_name:
+            management_ip = i['NetworkDevice']['ManagementIp']
+    return management_ip
+
+
+
